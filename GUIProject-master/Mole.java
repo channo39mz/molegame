94e9b7d6886c3735a792aa7422e8cd94f2b104a3
22p21n0056-chan <60224796+channo39mz@users.noreply.github.com>
2023-02-28 14:43:27 +0700

    Add files via upload

---
+++ GUIProject-master/Mole.java
@@ -0,0 +1,46 @@
+import javax.swing.*;
+import java.awt.*;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+
+public class Mole extends JButton {
+    public Icon holepic;
+    public Integer Id = 0;
+    public Icon el;
+    AllButtonListener bl = new AllButtonListener();
+    public Mole(){
+        try{
+			holepic = new ImageIcon("hole.png");
+			el = new ImageIcon("el.png");
+		}
+		catch(Exception e){
+			System.out.println(e);
+		}
+       
+        this.setPreferredSize(new Dimension(100,100));
+        this.setIcon(holepic);
+        this.setBackground(Color.green);
+        this.addActionListener(bl);
+    }  
+    public void toel(){
+        this.setIcon(el);
+    }
+    public void tohole(){
+        this.setIcon(holepic);
+    }
+    public void setID(int id){
+        this.Id = id;
+    }
+    public int getID(){
+        return(Id);
+    }
+    private class AllButtonListener implements ActionListener{
+        @Override
+        public void actionPerformed(ActionEvent ev){
+            // JButton source = (JButton)ev.getSource();
+            System.out.println(getID());
+        }
+    }
+}
